c7ea4be4ae33191f53087749a4e3088f1b4cea6d
Debarup <87480898+debarup24@users.noreply.github.com>
2024-11-28 21:51:41 +0530

    AI Trip Planner

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -3,7 +3,10 @@ import { Button } from "../ui/button";
 
 const Header = () => {
   return (
-    <div className="p-4 w-full shadow-sm shadow-gray-200 flex justify-between items-center px-10">
+    <div
+      className="p-4 w-full shadow-sm shadow-gray-100
+     flex justify-between items-center px-10"
+    >
       <img src=" /logo.svg " alt="logo" />
       <div className="flex flex-row">
         <Button>Sign In</Button>
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 const CreateTrip = () => {
-  return <div className="">CreateTrip</div>;
+  return (
+    <div className="sm:px-10 md:px-32 lg:px-56 px-5 mt-10">
+      CreateTrip
+      <h2 className="font-bold text-3xl">Tell us your travel preferences</h2>
+      <p className="mt-3  text-xl text-gray-500">
+        Just provide some basic information, and our trip planner will generate
+        a customized itinerary based on your preferences.
+      </p>
+      <div className="mt-20">
+        <div>
+          <h2 className="text-xl my-3 font-medium ">
+            What is destination of choice?
+          </h2>
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateTrip;
